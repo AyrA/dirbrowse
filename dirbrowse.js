9a07c9b230e5c18adcf605b07ed0cb554fcd66d8
Kevin Gut <kevin.gut@bitm.sg>
2017-10-04 20:31:45 +0200

    Parameter 'x' is needed in another project

--- dirbrowse.js
+++ dirbrowse.js
@@ -65,45 +65,44 @@ function getError(code, ip, body) {
 
 //Handle Directory listing as async (which is a pain in the ass to be honest)
 function listDir(directory, url, ip, cb) {
-	var readDone = function (e, x) {
-		if (!e) {
+	var readDone = function (err, dirlist) {
+		if (!err) {
 			var text = "<a class='dir-link dir-up' href='..'>..</a><br />";
-			for (var key in x) {
-				text += "<a class='"+(x[key].isDirectory()?"dir":"file")+"-link' href='" + encodeURIComponent(key) + "/'>" + key + "</a>";
+			for (var key in dirlist) {
+				text += "<a class='" + (dirlist[key].isDirectory() ? "dir" : "file") + "-link' href='" + encodeURIComponent(key) + "/'>" + key + "</a>";
 			}
 			cb(null, pages.dir.replace(/\[ROOT\]/g, "/" + url).replace(/\[CLUSTER\]/g, cluster.worker.id).replace(/\[IP\]/g, ip).replace(/\[BODY\]/g, text));
 		} else {
-			cb(e, null);
+			cb(err, null);
 		}
-	}
+	};
 
 	if (cache[directory] && Date.now() - cache[directory].age < 60000) {
 		readDone(null, cache[directory].data);
 	} else {
 		console.log("Building cache for", directory);
-		fs.readdir(directory, function (e, d) {
-			if (!e) {
+		fs.readdir(directory, function (err, dirlist) {
+			if (!err) {
 				cache[directory] = {
 					data: {}
 				};
-				var data = d.concat([]);
 				var proc = function () {
-					if (data.length > 0) {
-						var dir = data.shift();
-						fs.lstat(path.join(directory, dir), function (e, x) {
-							if (!e) {
-								cache[directory].data[dir] = x;
+					if (dirlist.length > 0) {
+						var dir = dirlist.shift();
+						fs.lstat(path.join(directory, dir), function (err, stat) {
+							if (!err) {
+								cache[directory].data[dir] = stat;
 							}
 							proc();
 						});
 					} else {
-						readDone(e, cache[directory].data);
+						readDone(err, cache[directory].data);
 					}
 				};
 				proc();
 				cache[directory].age = Date.now();
 			} else {
-				readDone(e, null);
+				readDone(err, null);
 			}
 		});
 	}
@@ -139,18 +138,18 @@ function request(req, res) {
 			path.normalize(path.join(args.root, requestdir)) :
 			path.normalize(path.join(customdir, requestdir.substr(2)));
 
-		fs.stat(urlpath, function (e, x) {
-			if (!e) {
-				if (x.isDirectory()) {
+		fs.stat(urlpath, function (err, stat) {
+			if (!err) {
+				if (stat.isDirectory()) {
 					//Show Directory
-					listDir(urlpath, requestdir, IP, function (e, x) {
-						if (!e) {
+					listDir(urlpath, requestdir, IP, function (err, dirlistHtml) {
+						if (!err) {
 							//Write Directory result
 							res.writeHead(200, {
 								"Content-Type": "text/html",
 								Expires: expiration.dir
 							});
-							res.end(x);
+							res.end(dirlistHtml);
 						} else {
 							//Error accessing directory
 							res.writeHead(403, {
@@ -209,78 +208,98 @@ function request(req, res) {
 	}
 }
 
-function numToCPU(x) {
-	if (isNaN(x)) {
-		//Safety override
-		if ((x + "").substr(0, 1) === '!' && parseInt((x + "").substr(1)) > 0) {
-			return parseInt((x + "").substr(1));
-		} else {
-			return null;
+function numToCPU(number) {
+	if (typeof(number) === typeof("") || typeof(number) === typeof(0)) {
+		if (isNaN(number)) {
+			//Safety override
+			if (number.substr(0, 1) === '!' && parseInt(number.substr(1)) > 0) {
+				return parseInt(number.substr(1));
+			} else {
+				return null;
+			}
 		}
-	}
-	//negative workers tell the system how many CPU cores *not* to use
-	else if (x === 0) {
-		return numCPUs;
-	} else if (x < 0) {
-		if (numCPUs + x < 1) {
-			return null;
-		} else {
-			return numCPUs + x;
+		//negative workers tell the system how many CPU cores *not* to use
+		else if (number === 0) {
+			//Use CPU count if number is 0
+			return numCPUs;
+		} else if (number < 0) {
+			if (numCPUs + number < 1) {
+				//If number is negative it's absolute value can't be equal or larger than the CPU count
+				return null;
+			} else {
+				//Number is the amount of cores to leave unused
+				return numCPUs + number;
+			}
 		}
+		//Number can be used as-is
+		return number;
 	}
-	return x;
+	//Return null on invalid argument
+	return null;
 }
 
 ////////////////////
 //Startup sequence//
 ////////////////////
 
-config.load(function (e, data) {
+//Load configuration
+config.load(function (err, data) {
 
-	if (e && !data) {
-		console.error("Error loading config file:", e.message);
+	//Error and no data means that the config exists and is invalid
+	if (err && !data) {
+		console.error("Error loading config file:", err.message);
 		process.exit(1);
 	}
-	if (e && data) {
+	//Error and data means that a default was created and loaded
+	if (err && data) {
 		console.log("config.json not available. Saving defaults. Please edit");
 		config.saveDefault();
 	}
+	//Set IP and Port if Argument for IP was not provided
 	if (!args.ip) {
 		args.ip = data.server.ip;
 		args.port = data.server.port;
 	}
+	//Set root if Argument was not provided
 	if (!args.root) {
 		if (data.root) {
 			args.root = data.root;
 		} else {
-			console.log("Root directory not specified");
-			process.exit(0);
+			//Config has no root either
+			console.log("Root directory not specified ad command line or config.json");
+			process.exit(1);
 		}
 	}
 	if (typeof(args.workers) != typeof(0)) {
+		//worker Argument not provided
 		args.workers = numToCPU(data.workers);
 	} else {
+		//worker Argument provided
 		args.workers = numToCPU(args.workers);
 	}
+	//No key specified and TLS is enabled in config.json
 	if (!args.key && data.server.tls) {
 		try {
+			//Try to read configured key and cert files
 			args.key = fs.readFileSync(data.server.key);
 			args.cert = fs.readFileSync(data.server.cert);
-		} catch (e) {
-			console.error("TLS enabled but files unreadable:", e.message);
+		} catch (err) {
+			console.error("TLS enabled in config.json but files unreadable:", err.message);
 			exit(1);
 		}
 	}
 
+	//Set ipcheck Parameters from config
 	ipcheck.enabled = data.access.enabled;
 	ipcheck.list = data.access.whitelist;
 	ipcheck.maxage = data.access.maxage;
 
+	//The master itself will not host a Web Server itself
 	if (cluster.isMaster) {
-		console.log("Simple HTTP Directory browser with range support");
-
+		console.log("Simple HTTP Directory browser with range support | https://github.com/AyrA/dirbrowse");
 		console.log("INFO: Root Directory:", args.root);
 
+		//Warn user about potentially unsafe configuration
 		if (!ipcheck.enabled) {
 			console.log("WARN: IP restriction is disabled");
 		}
@@ -288,16 +307,19 @@ config.load(function (e, data) {
 			console.log("WARN: Using unencrypted HTTPS");
 		}
 
+		//Build URL the Server listens at.
 		console.log("Starting Listener on http" + (args.key && args.cert ? "s" : "") + "://" + args.ip + ":" + args.port);
 		//Show when a worker comes online
 		cluster.on("online", function (worker) {
+			//Report when a worker was started
 			console.log("worker", worker.id, "started");
 		});
 		//Show when a worker exits
 		cluster.on("exit", function (worker, code, signal) {
-			//Treat non-zero code as unexpected exit and restart worker
+			//Treat non-zero code as unexpected exit and fire up new worker
 			if (code !== 0) {
 				console.error("worker", worker.id, "died without 'success' code. Signal:", signal);
+				//Don't fire up worker immediately or bad things happen if he crashes instantly
 				setTimeout(cluster.fork, 500);
 			} else {
 				console.log("Worker", worker.id, "stopped gracefully");
@@ -317,27 +339,27 @@ config.load(function (e, data) {
 				args.cert = fs.readFileSync(data.server.cert);
 			}
 
+			var server;
+
 			//check if HTTPS
 			if (args.key && args.cert) {
-				//Create encrypted HTTPS server
-				var server = https.createServer({
+				//Create HTTPS server
+				server = https.createServer({
 						key: args.key,
 						cert: args.cert
-					}, request).listen(args.port, args.ip).on("error", function (e) {
-						console.error("Unable to start listener. Error:", e.message);
-						process.exit(0);
-					});
-				server.timeout = 0;
+					}, request);
 			} else {
 				//Create unencrypted HTTP server
-				var server = http.createServer(request).listen(args.port, args.ip).on("error", function (e) {
-						console.error("Unable to start listener. Error:", e.message);
-						process.exit(0);
-					});
+				server = http.createServer(request);
 				server.timeout = 0;
 			}
-		} catch (e) {
-			console.error("Unable to start listener. Error:", e.message);
+			server.listen(args.port, args.ip).on("error", function (err) {
+				console.error("Unable to start listener. Error:", err.message);
+				process.exit(0);
+			});
+			server.timeout = 0;
+		} catch (err) {
+			console.error("Unable to start listener. Error:", err.message);
 			process.exit(0);
 		}
 	}
